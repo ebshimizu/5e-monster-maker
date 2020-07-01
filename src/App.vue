529f6ef083ed8a3839e5a79d87b4a0c56121c737
Evan Shimizu <ebshimizu@gmail.com>
2020-07-01 19:03:44 -0400

    scrollbar and layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-toolbar-title>5e Monster Maker</v-toolbar-title> 
+  <v-app class="app">
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>5e Monster Maker</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
       <v-container fluid>
         <v-row no-gutters>
-          <v-col cols="8"><data-entry /></v-col>
-          <v-col cols="4" class="px-6 py-2"><render /></v-col>
+          <v-col cols="8" class="full-height pa-1"><data-entry /></v-col>
+          <v-col cols="4" class="full-height px-6 py-2"><render /></v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -20,15 +16,15 @@
 </template>
 
 <script>
-import DataEntry from './components/DataEntry'
-import Render from './components/Render'
+import DataEntry from './components/DataEntry';
+import Render from './components/Render';
 
 export default {
   name: 'App',
 
   components: {
     DataEntry,
-    Render
+    Render,
   },
 
   data: () => ({
@@ -36,3 +32,41 @@ export default {
   }),
 };
 </script>
+
+<style>
+::-webkit-scrollbar {
+  display: none;
+}
+
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-track {
+  margin-left: 2px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #505050;
+  border: solid 2px #202020;
+  border-radius: 7px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #b6b6b6;
+}
+
+.full-height::-webkit-scrollbar {
+  display: block;
+}
+
+.full-height {
+  height: calc(100vh - 90px);
+  overflow: auto;
+}
+
+.app {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
