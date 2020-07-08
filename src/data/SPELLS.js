8196da0472485435674c7fa1ac22571fd5b7e9c1
Evan Shimizu <ebshimizu@gmail.com>
2020-07-08 16:49:57 -0400

    spell recharge type update
    more templates n stuff

--- src/data/SPELLS.js
+++ src/data/SPELLS.js
@@ -4,6 +4,9 @@ export const AT_WILL_DEFAULT_RATES = {
   DAY: 'day',
   DAY_EACH: 'day each',
   AT_WILL: 'at will',
+  LONG_REST: 'long rest',
+  SHORT_REST: 'short rest',
+  LONG_OR_SHORT: 'long or short rest'
 };
 
 export default ALL;
