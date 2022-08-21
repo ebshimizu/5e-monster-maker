134c59833cbfab5b8b10d30f116f5d08cc30a299
Evan Shimizu <ebshimizu@gmail.com>
2022-08-21 11:04:03 -0700

    workflow  test

--- quasar.config.js
+++ quasar.config.js
@@ -59,7 +59,8 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
-      publicPath: '/5emm',
+      // TODO: when moving to master, change this prefix
+      publicPath: '/5emm-stage',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
