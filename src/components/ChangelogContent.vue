3f0d15252dd7b8c4c4d6f30ae759aae564a21ce1
Evan Shimizu <ebshimizu@gmail.com>
2024-12-19 11:12:58 -0800

    resolve #138

--- src/components/ChangelogContent.vue
+++ src/components/ChangelogContent.vue
@@ -1,6 +1,22 @@
 <template>
   <q-timeline color="secondary">
     <q-timeline-entry
+      title="v2.2.11"
+      subtitle="Bugfix - 12/19/2024"
+      icon="build"
+      color="primary"
+    >
+      <div class="text-body2">
+        <ul>
+          <li>
+            <github-issue-link :issue="138" /> Fixed infinite loop in CR
+            calculation when legendary action cost was 0. The UI should also now
+            prevent you from setting a cost of 0.
+          </li>
+        </ul>
+      </div>
+    </q-timeline-entry>
+    <q-timeline-entry
       title="v2.2.10"
       subtitle="Bugfix - 11/26/2024"
       icon="build"
